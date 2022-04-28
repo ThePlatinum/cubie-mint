a39643c0f02710512b3af29193ae8293c8d5d026
Itsme <54501125+varun13603@users.noreply.github.com>
2022-04-28 20:09:28 +0530

    Update 20.js

--- pages/api/cubies/20.js
+++ pages/api/cubies/20.js
@@ -4,7 +4,7 @@ export default function handler(req, res) {
   res.status(200).json({
     "name"  : "Cubie #20",
     "rarity": "Epic",
-    "power" : "2,5",
+    "power" : "2.5",
     "image" : "https://ipfs.filebase.io/ipfs/bafkreigni3tcjdw2okysp4g2f5mu3qzm2j3gtuzqagl7ksgvbq6oog6dai"
   })
 }
