6ac108fa24102482585d63f9447ea0df67d6d945
Itsme <54501125+varun13603@users.noreply.github.com>
2022-04-28 20:40:23 +0530

    Update 47.js

--- pages/api/cubies/47.js
+++ pages/api/cubies/47.js
@@ -3,8 +3,8 @@
 export default function handler(req, res) {
   res.status(200).json({
     "name"  : "Cubie #47",
-    "rarity": "Epic",
-    "power" : "3",
-    "image" : "https://drive.google.com/uc?export=view&id=1bkKxWGdFnfD7iggFzwxRYvxtAgmnDb_D"
+    "rarity": "Unique",
+    "power" : "1.5",
+    "image" : "https://ipfs.filebase.io/ipfs/bafkreifcqbkm5kb5qtpxpd6utacozv6grjvqgdbcgzvvhvj756jxlk5g7i"
   })
 }
