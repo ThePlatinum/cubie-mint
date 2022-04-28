39b0db357b93d3ede8b6abeafb03063905479d62
Itsme <54501125+varun13603@users.noreply.github.com>
2022-04-28 20:56:59 +0530

    Update 59.js

--- pages/api/cubies/59.js
+++ pages/api/cubies/59.js
@@ -3,8 +3,8 @@
 export default function handler(req, res) {
   res.status(200).json({
     "name"  : "Cubie #59",
-    "rarity": "Epic",
-    "power" : "3",
-    "image" : "https://drive.google.com/uc?export=view&id=1bkKxWGdFnfD7iggFzwxRYvxtAgmnDb_D"
+    "rarity": "Unique",
+    "power" : "1.5",
+    "image" : "https://ipfs.filebase.io/ipfs/bafkreiffg4lhxgqipn5rzodz5rddbjju4sl7wcjzv75zhkpkn3735z4mpi"
   })
 }
