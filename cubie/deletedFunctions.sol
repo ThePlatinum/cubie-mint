1abdc26c535637db6df553e405d7ed0d708d3138
Platinum <platinumemirate@gmail.com>
2022-04-29 18:57:06 +0100

    contract deleted functions

---
+++ cubie/deletedFunctions.sol
@@ -0,0 +1,70 @@
+
+/**
+ * @title Roles
+ * @dev Library for managing addresses assigned to a Role.
+ */
+library Roles {
+    struct Role {
+        mapping (address => bool) bearer;
+    }
+
+    /**
+     * @dev Give an account access to this role.
+     */
+    function add(Role storage role, address account) internal {
+        require(!has(role, account), "Roles: account already has role");
+        role.bearer[account] = true;
+    }
+
+    /**
+     * @dev Remove an account's access to this role.
+     */
+    function remove(Role storage role, address account) internal {
+        require(has(role, account), "Roles: account does not have role");
+        role.bearer[account] = false;
+    }
+
+    /**
+     * @dev Check if an account has this role.
+     * @return bool
+     */
+    function has(Role storage role, address account) internal view returns (bool) {
+        require(account != address(0), "Roles: account is the zero address");
+        return role.bearer[account];
+    }
+}
+
+contract MinterRole is Context {
+    using Roles for Roles.Role;
+
+    event MinterAdded(address indexed account);
+
+    Roles.Role private _minters;
+
+    constructor () internal {
+        _addMinter(_msgSender());
+    }
+
+    modifier onlyMinter() {
+        require(isMinter(_msgSender()), "MinterRole: caller does not have the Minter role");
+        _;
+    }
+
+    function isMinter(address account) public view returns (bool) {
+        return _minters.has(account);
+    }
+
+    function addMinter(address account) public onlyMinter {
+        _addMinter(account);
+    }
+
+    function _addMinter(address account) internal {
+        _minters.add(account);
+        emit MinterAdded(account);
+    }
+}
+
+
+    function aTokenURI(uint256 tokenId, string memory _tokenURI) public onlyOwner {
+      _setTokenURI(tokenId, _tokenURI);
+    }
