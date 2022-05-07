55c5b5a71c975dcfde5743d39e9ccffc79c5c73c
Itsme <54501125+varun13603@users.noreply.github.com>
2022-05-07 15:28:41 +0530

    Update 12.js

--- pages/api/cubies/12.js
+++ pages/api/cubies/12.js
@@ -5,6 +5,6 @@ export default function handler(req, res) {
     "name"  : "Cubie #12",
     "rarity": "Normal",
     "power" : "1",
-    "image" : "https://gateway.btfs.io/btfs/QmXM4nvFR9MMNcoPBUopxcKthiKYRYgJXSZEv3VrWY9zfc"
+    "image" : "https://gateway.btfs.io/btfs/QmcvhxRAZrGpXobcSNkRwy6L5bCThaScwGBczwZF6KqT4b"
   })
 }
